0704a30af8b3e3945544fa22d4607f5b25628de0
niupeng <2368235544@qq.com>
2025-12-01 10:43:03 +0800

    测试在github 打包+部署页面

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}李婷婷</h1>
+    <h1>测试在github 打包+部署页面</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
